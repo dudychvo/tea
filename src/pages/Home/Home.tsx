288dc975d2580440c0cc2bfebb073312953cb31e
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2026-01-16 18:09:03 +0000

    update home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,24 @@
 import { Header } from '../../components/Header/Header';
 import { HeroHero } from '../../components/HomeHero/HomeHero';
 import { HomeQuiz } from '../../components/HomeQuiz/HomeQuiz';
+import { Footer } from '../../components/Footer/Footer';
 
 import styles from './Home.module.scss';
 
 export const Home = () => {
   return (
     <>
-      <div className={styles.backgroundWrapper}>
-        <div className={styles.topContainer}>
-          <Header />
-          <HeroHero />
+      <div className={styles.homeWrapper}>
+        <div className={styles.heroWrapper}>
+          <div className={styles.container}>
+            <Header />
+            <HeroHero />
+          </div>
         </div>
-        <HomeQuiz />
+        <div className={styles.container}>
+          <HomeQuiz />
+          <Footer />
+        </div>
       </div>
     </>
   );
