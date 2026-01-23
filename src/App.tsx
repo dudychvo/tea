39eb6a96f30990b18a0e5267b4aa96b86fc063eb
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2026-01-23 14:41:46 +0000

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home/Home';
 import { Guide } from './pages/Guide/Guide';
+import { Shop } from './pages/Shop/Shop';
 
 import './App.scss';
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/guide' element={<Guide />} />
+      <Route path='/shop' element={<Shop />} />
     </Routes>
   );
 }
