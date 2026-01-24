5ff96daf8d054547321ac8975d5d581c2e427a4d
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2026-01-24 20:25:20 +0000

    update header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
 
 import { AuthModal } from '../AuthModal/AuthModal';
+import { CartModal } from '../CartModal/CartModal';
+import { QuizModal } from '../QuizModal/QuizModal';
 
 import styles from './Header.module.scss';
 
@@ -33,10 +35,10 @@ export const Header = () => {
               </NavLink>
             </li>
             <li>
-              <a href='#'>Quiz</a>
+              <QuizModal />
             </li>
             <li>
-              <a href='#'>Cart</a>
+              <CartModal />
             </li>
           </ul>
         </nav>
